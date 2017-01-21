2b0e86187aa379dcf3d959ce290ba724852fca0b
Siddhartha Nutulapati <sidgonuts@gmail.com>
2017-01-21 11:46:15 -0500

    firebase issues

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var config = {
+  apiKey: "AIzaSyD4YwHeE2pKlDAWTeBfdEJsIIZb4WLAG-s",
+  authDomain: "sidsjokes-adb9b.firebaseapp.com",
+  databaseURL: "https://sidsjokes-adb9b.firebaseio.com",
+  storageBucket: "sidsjokes-adb9b.appspot.com",
+  messagingSenderId: "383575493897"
+};
+firebase.initializeApp(config);
+
 var app = angular.module('sidsJokesApp', ['firebase']);
 
 app.controller('formCtrl', function($scope, $firebaseArray) {
