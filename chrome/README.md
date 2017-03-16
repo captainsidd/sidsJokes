70f98177667b34fd02708583b7941dacb4403836
Siddhartha Nutulapati <sidgonuts@gmail.com>
2017-03-16 16:02:25 -0400

    add chrome extension files

---
+++ chrome/README.md
@@ -0,0 +1,8 @@
+# sidsjokes-chrome
+
+A chrome extension that does what this [twitter bot](https://github.com/sidgonuts/sidsjokes) does.
+
+Doing this so other people can submit bad jokes, and would also make it easier to
+add jokes to queue - I don't want to type out http://captainsidd.com/sidsJokes everytime.
+
+Also, it'd be cool to make a Chrome Extension. 
---
+++ chrome/popup.js
@@ -0,0 +1,22 @@
+var config = {
+  apiKey: "AIzaSyD4YwHeE2pKlDAWTeBfdEJsIIZb4WLAG-s",
+  authDomain: "sidsjokes-adb9b.firebaseapp.com",
+  databaseURL: "https://sidsjokes-adb9b.firebaseio.com",
+  storageBucket: "sidsjokes-adb9b.appspot.com",
+  messagingSenderId: "383575493897"
+};
+firebase.initializeApp(config);
+
+var app = angular.module('sidsJokesApp', ['firebase']);
+
+app.controller('formCtrl', function($scope, $firebaseArray) {
+  var ref = firebase.database().ref().child('tweets');
+  $scope.tweets = $firebaseArray(ref);
+  $scope.submit = function() {
+    if($scope.joke.length <= 140) {
+      $scope.tweets.$add({
+        joke: $scope.joke
+      });
+    }
+  };
+});
